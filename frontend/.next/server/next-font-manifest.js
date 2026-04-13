e38753aaf0186861f7f6c4802352e6cf901817ed
skyblood <edgar.rojas@blackmoon.com.co>
2026-04-12 22:05:42 -0500

    seguridad

---
+++ frontend/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/blackmoon/Library/CloudStorage/GoogleDrive-edgar.rojas@blackmoon.com.co/My Drive/PRAIE/praie-agent/frontend/src/app/layout":["static/media/13971731025ec697-s.p.woff2","static/media/eaead17c7dbfcd5d-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
